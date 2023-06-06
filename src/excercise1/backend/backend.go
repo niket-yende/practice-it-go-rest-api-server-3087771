08c83de335bca9aabf9ca95e68953d483e6c2031
Niket Yende <niket6@gmail.com>
2023-06-06 21:54:03 +0530

    feat: completed excercise 3b

--- src/excercise1/backend/backend.go
+++ src/excercise1/backend/backend.go
@@ -7,11 +7,13 @@ import (
 	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/gorilla/mux"
 )
 
 type App struct {
-	DB   *sql.DB
-	Port string
+	DB   	*sql.DB
+	Port 	string
+	Router 	*mux.Router
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -25,10 +27,16 @@ func (a *App) Initialize() {
 	}
 
 	a.DB = DB
+	a.Router = mux.NewRouter()
+	a.initializeRouter()
+}
+
+func (a *App) initializeRouter() {
+	a.Router.HandleFunc("/", helloWorld)
 }
 
 func (a *App) Run() {
 	http.HandleFunc("/", helloWorld)
 	fmt.Println("Server started and listening on port ", a.Port)
-	log.Fatal(http.ListenAndServe(a.Port, nil))
+	log.Fatal(http.ListenAndServe(a.Port, a.Router))
 }
--- src/excercise1/backend/go.mod
+++ src/excercise1/backend/go.mod
@@ -3,3 +3,5 @@ module example.com/backend
 go 1.20
 
 require github.com/mattn/go-sqlite3 v1.14.17
+
+require github.com/gorilla/mux v1.8.0
